a7798f3d1a114d5efa3653be6c6f8c0e4d3ec946
Finn Brown <43194191+finnatsea@users.noreply.github.com>
2021-12-14 01:04:49 -0500

    more installs

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 
+import styled from "styled-components";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <p>This is Finn&apos;s ez-starter repo. Take it eazy man!</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
+const AppHeader = styled.div``;
